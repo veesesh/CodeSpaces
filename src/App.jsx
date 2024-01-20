18779a5da499a4c4763405a4a1f9d1479704a9e9
Vee <vee1912566@gmail.com>
2024-01-20 19:50:44 +0530

    Frost hacks

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+
+var data = JSON.stringify({
+  "collection": "<COLLECTION_NAME>",
+  "database": "sample_training",
+  "dataSource": "Cluster0",
+  "projection": {
+      "_id": 1
+  }
+});
+          
+var config = {
+  method: 'post',
+  url: 'https://ap-south-1.aws.data.mongodb-api.com/app/data-zvpzn/endpoint/data/v1/action/findOne',
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Request-Headers': '*',
+    'Authorization': 'Bearer <ACCESS_TOKEN>',
+  },
+  data: data
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(function () {
+    fetch(
+      "https://realm.mongodb.com/api/client/v2.0/app/data-zvpzn/auth/providers/local-userpass/login",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "curiousvee19@gmail.com",
+          password: "wJdwpaI4SfxSdvFI",
+        }),
+      }
+    )
+      .then((res) => res.json())
+      .then((re) => console.log(re));
+  }, []);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <>Test</>;
 }
 
-export default App
+export default App;
+
+
+
+
