aeccd6a2890698b54e9a84e703d6b254ce439150
Vee <vee1912566@gmail.com>
2024-01-20 21:56:56 +0530

    Frost

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,129 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
-var data = JSON.stringify({
-  "collection": "<COLLECTION_NAME>",
-  "database": "sample_training",
+
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+myHeaders.append("Access-Control-Request-Headers", "null");
+myHeaders.append("api-key", "501TNEVCcPCbuNjBDKVAqbU3cMQ30SwJHBBd3DzMo0Pc2aUsEsPiw9bBfEzD7pyG");
+
+var raw = JSON.stringify({
   "dataSource": "Cluster0",
-  "projection": {
-      "_id": 1
+  "database": "cvbdvb",
+  "collection": "dfgsdfgdsfg",
+  "document": {
+    "text": "Veeeee"
   }
 });
-          
-var config = {
-  method: 'post',
-  url: 'https://ap-south-1.aws.data.mongodb-api.com/app/data-zvpzn/endpoint/data/v1/action/findOne',
-  headers: {
-    'Content-Type': 'application/json',
-    'Access-Control-Request-Headers': '*',
-    'Authorization': 'Bearer <ACCESS_TOKEN>',
-  },
-  data: data
+
+var requestOptions = {
+  mode: "no-cors",
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
 };
 
+fetch("https://ap-south-1.aws.data.mongodb-api.com/app/data-zvpzn/endpoint/data/v1/action/insertOne", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const inputField = useRef(null);
   useEffect(function () {
-    fetch(
-      "https://realm.mongodb.com/api/client/v2.0/app/data-zvpzn/auth/providers/local-userpass/login",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          username: "curiousvee19@gmail.com",
-          password: "wJdwpaI4SfxSdvFI",
-        }),
-      }
-    )
-      .then((res) => res.json())
-      .then((re) => console.log(re));
+    // fetch(
+    //   "https://realm.mongodb.com/api/client/v2.0/app/data-zvpzn/auth/providers/local-userpass/login",
+    //   {
+    //     method: "POST",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify({
+    //       username: "curiousvee19@gmail.com",
+    //       password: "wJdwpaI4SfxSdvFI",
+    //     }),
+    //   }
+    // )
+    //   .then((res) => res.json())
+    //   .then((re) => console.log(re));
   }, []);
 
-  return <>Test</>;
+  function handleClick() {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("Access-Control-Request-Headers", "null");
+    myHeaders.append(
+      "api-key",
+      "501TNEVCcPCbuNjBDKVAqbU3cMQ30SwJHBBd3DzMo0Pc2aUsEsPiw9bBfEzD7pyG"
+    );
+
+    var raw = JSON.stringify({
+      dataSource: "Cluster0",
+      database: "cvbdvb",
+      collection: "dfgsdfgdsfg",
+      document: {
+        name: inputValue,
+      },
+    });
+
+    var requestOptions = {
+      mode: "no-cors",
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://ap-south-1.aws.data.mongodb-api.com/app/data-zvpzn/endpoint/data/v1/action/insertOne",
+      requestOptions
+    )
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.log("error", error));
+  }
+
+  return (
+    <>
+      <label htmlFor="name"></label>
+      <input
+        type="text"
+        id="name"
+        value={inputValue}
+        ref={inputField}
+        onChange={() => setInputValue(inputField.current.value)}
+      />
+      <button onClick={handleClick}>Click Me</button>
+    </>
+  );
 }
 
 export default App;
 
+// var myHeaders = new Headers();
+// myHeaders.append("Content-Type", "application/json");
+// myHeaders.append("Access-Control-Request-Headers", "*");
+// myHeaders.append("api-key", "501TNEVCcPCbuNjBDKVAqbU3cMQ30SwJHBBd3DzMo0Pc2aUsEsPiw9bBfEzD7pyG");
 
+// var raw = JSON.stringify({
+//   "dataSource": "Cluster0",
+//   "database": "cvbdvb",
+//   "collection": "dfgsdfgdsfg",
+//   "document": {
+//     "text": "Hello, world!"
+//   }
+// });
 
+// var requestOptions = {
+//   method: 'POST',
+//   headers: myHeaders,
+//   body: raw,
+//   redirect: 'follow'
+// };
 
+// fetch("https://ap-south-1.aws.data.mongodb-api.com/app/data-zvpzn/endpoint/data/v1/action/insertOne", requestOptions)
+//   .then(response => response.text())
+//   .then(result => console.log(result))
+//   .catch(error => console.log('error', error));
